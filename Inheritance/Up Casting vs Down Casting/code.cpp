3832393a14e3a3a2eb3e656b438e0ed7eb8ff89f
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-03-07 11:54:08 +0200

    Create code.cpp

---
+++ Inheritance/Up Casting vs Down Casting/code.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class clsPerson
+{
+public: 
+	string FullName = "Ragdha Elgaidi"; 
+}; 
+
+class clsEmployee: public clsPerson
+{
+public:
+	string Title = "ml engineer";
+};
+
+int main() 
+{
+	clsEmployee Employee1;   
+	cout << Employee1.FullName << endl;
+
+	////upcasting//this will convert employee to person.
+	clsPerson * Person1 = &Employee1;    //up casting
+	cout << Person1->FullName << endl;
+	
+	//clsPerson Person2;
+	//cout << Person2.FullName << endl;
+	
+	////downcasting : you cannot convert person to employee
+	//clsEmployee* Employee2 = &Person2;    
+	
+	system("pause>0");
+
+	return 0;
+}
